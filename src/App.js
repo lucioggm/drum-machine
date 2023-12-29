3d343d894928dcf7c26a0ed834f7dcdb59126358
Luciano <lucioggm210@gmail.com>
2023-12-28 21:58:54 -0600

    Funcional, sin estilos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [buttonSelected, setButtonSelected] = useState();
+  const [soundSelected, setSoundSelected] = useState("");
+  const buttonLetters = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+  const soundList = ["Alien", "Beep", "Boom", "Coin", "Fun", "Hat", "Kick", "Random", "Rare"];
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      const tecla = e.key.toUpperCase();
+      setButtonSelected(tecla);
+      setSoundSelected(relecionadorEntreLetraSonido(tecla));
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  function relecionadorEntreLetraSonido(letra) {
+    const soundMapping = {
+      "Q": "Alien",
+      "W": "Beep",
+      "E": "Boom",
+      "A": "Coin",
+      "S": "Fun",
+      "D": "Hat",
+      "Z": "Kick",
+      "X": "Random",
+      "C": "Rare"
+    };
+
+    return soundMapping[letra] || null;
+  }
+
+  function Display() {
+    return <div id={"display"}>{soundSelected}</div>;
+  }
+
+  useEffect(() => {
+    if (buttonLetters.includes(buttonSelected)) {
+      const audio = document.getElementById(buttonSelected);
+      audio.play().catch(err => {
+        console.error("Error al reproducir el sonido:", err);
+      });
+    }
+  }, [buttonSelected]);
+
+  function handleClick(e) {
+    const nuevo = e.currentTarget.innerText;
+    setButtonSelected(nuevo);
+    setSoundSelected(relecionadorEntreLetraSonido(nuevo));
+  }
+
+  function Pad() {
+    return buttonLetters.map((key, index) => (
+        <button className={"drum-pad"} key={index} onClick={handleClick}>
+          {key}
+          <audio id={key} src={`/${soundList[index]}.wav`}></audio>
+        </button>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id={"drum-machine"}>
+        <h1>Hola</h1>
+        <Pad />
+        <Display />
+      </div>
   );
 }
 
